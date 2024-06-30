ab259d7d68197ac891268d06613697938f8d61b1
ChinniIndravathi <141818767+ChinniIndravathi@users.noreply.github.com>
2024-06-30 13:41:45 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,45 @@
+var TodoApp = /** @class */ (function () {
+    function TodoApp() {
+        this.todoTasks = [];
+        this.taskInputHTML = document.getElementById("taskInput");
+        this.todoItemsHTML = document.getElementById("todoItems");
+    }
+    TodoApp.prototype.addNewTask = function (taskText) {
+        var newTask = {
+            id: (new Date()).getTime(),
+            task: taskText,
+            completed: false
+        };
+        this.todoTasks.push(newTask);
+        this.renderTasks();
+        this.taskInputHTML.value = '';
+    };
+    TodoApp.prototype.markCompleted = function (id) {
+        var targetTask = this.todoTasks.filter(function (el) {
+            return el.id === id;
+        })[0];
+        if (targetTask) {
+            targetTask.completed = !targetTask.completed;
+        }
+        // TODO: send alert for invalid id
+        this.renderTasks();
+    };
+    TodoApp.prototype.removeTask = function (id) {
+        this.todoTasks = this.todoTasks.filter(function (el) {
+            return el.id !== id;
+        });
+        this.renderTasks();
+    };
+    TodoApp.prototype.renderTasks = function () {
+        var _this = this;
+        /*
+        convert the ts array into html list
+        */
+        this.todoItemsHTML.innerHTML = '';
+        this.todoTasks.forEach(function (todoTask) {
+            _this.todoItemsHTML.innerHTML += "\n                <li class=\"".concat(todoTask.completed ? 'completed' : 'notcompleted', "\">\n                    ").concat(todoTask.task, "\n                    <button id=\"done-button\" class=\"fas fa-check\" onclick=\"todoApp.markCompleted(").concat(todoTask.id, ")\"></button>\n                    <button id=\"delete-icon\" class=\"fas fa-trash-alt delete-icon\" onclick=\"todoApp.removeTask(").concat(todoTask.id, ")\"></button>\n                </li>\n            ");
+        });
+    };
+    return TodoApp;
+}());
+var todoApp = new TodoApp();
---
+++ todo.ts
@@ -0,0 +1,68 @@
+interface TodoTask{
+    id: number,
+    task: string,
+    completed: boolean
+}
+
+class TodoApp{
+    todoTasks: TodoTask[]
+    taskInputHTML: HTMLInputElement 
+    todoItemsHTML: HTMLUListElement
+
+    constructor(){
+        this.todoTasks = []
+        this.taskInputHTML = document.getElementById("taskInput") as HTMLInputElement
+        this.todoItemsHTML = document.getElementById("todoItems") as HTMLUListElement
+    }
+    
+    addNewTask(taskText: string){
+        const newTask:TodoTask  = {
+            id: (new Date()).getTime(),
+            task:taskText,
+            completed : false
+        }
+        
+          
+        this.todoTasks.push(newTask)
+        this.renderTasks()
+        this.taskInputHTML.value = ''
+        
+        
+    }
+
+    markCompleted(id: number){
+        const targetTask = this.todoTasks.filter((el) =>{
+            return el.id === id
+        })[0]
+        if(targetTask){
+            targetTask.completed = !targetTask.completed;
+        }
+        // TODO: send alert for invalid id
+        this.renderTasks()
+    }
+
+    removeTask(id: number){
+        this.todoTasks = this.todoTasks.filter((el) =>{
+            return el.id !== id
+        })
+        this.renderTasks()
+    }
+
+    renderTasks(){
+        /*
+        convert the ts array into html list
+        */
+        this.todoItemsHTML.innerHTML = ''
+        this.todoTasks.forEach((todoTask: TodoTask)=>{
+            this.todoItemsHTML.innerHTML += `
+                <li class="${todoTask.completed? 'completed': 'notcompleted'}">
+                    ${todoTask.task}
+                    <button id="done-button" class="fas fa-check" onclick="todoApp.markCompleted(${todoTask.id})"></button>
+                    <button id="delete-icon" class="fas fa-trash-alt delete-icon" onclick="todoApp.removeTask(${todoTask.id})"></button>
+                </li>
+            `
+        })
+    }
+}
+
+const todoApp = new TodoApp()
